35eed9b3c1fa824d22649f1dd927d4ef0740dd79
bhavishya-polisetti <bhavishyapolisetti@gmail.com>
2025-12-25 16:21:14 +0000

    chore(octofit): add tracker.populate_db management command wrapper

---
+++ octofit-tracker/backend/tracker/management/commands/populate_db.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.core.management import call_command
+
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **options):
+        try:
+            call_command('populate_test_data')
+            self.stdout.write(self.style.SUCCESS('Database populated via populate_test_data.'))
+        except Exception as exc:
+            raise CommandError(f'Error populating database: {exc}')
